a3a488e3d08db498e073ebb5d8b9cb8e1afd40b9
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-11-07 12:25:21 +0100

    Added - model for match

---
+++ backend/controllers/predictionController.js
@@ -0,0 +1,13 @@
+import PredictionModel from "../models/predictionModel.js";
+
+
+
+export const createPrediction = async (req, res) => {
+    try {
+        const {user_id, match_id, predicted_outcome, summary} = req.body;
+
+        const match = await 
+    } catch (error) {
+        
+    }
+}
---
+++ backend/models/matchModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const matchSchema = new mongoose.Schema({
+  match_id: { type: String, required: true, unique: true },
+  team_home: { type: String, required: true },
+  team_away: { type: String, required: true },
+  match_date: { type: Date, required: true },
+  status: { type: String, required: true },
+  result: { result: String },
+  competition: { type: String, required: true },
+});
+
+export default mongoose.model("Match", matchSchema);
