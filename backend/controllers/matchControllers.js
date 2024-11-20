3a225d2d96c1c74e780b9daec5a9516e637d594d
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-11-20 12:00:35 +0100

    Updated matchModel

--- backend/controllers/matchControllers.js
+++ backend/controllers/matchControllers.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import NodeCache from "node-cache";
 
 const API_BASE_URL = "https://api.football-data.org/v4/";
+
+// cache updates every 5 minutes
+const cache = new NodeCache({ stdTTL: 300 });
 
 const fetchAllMatches = async (req, res) => {
   const { league } = req.query;
@@ -8,6 +12,14 @@ const fetchAllMatches = async (req, res) => {
 
   if (!league) {
     return res.status(400).json({ message: "No league found" });
+  }
+
+  const cacheKey = `matches_${league}`;
+  const cachedData = cache.get(cacheKey);
+
+  if (cachedData) {
+    console.log("Serving from cache");
+    return res.status(200).json(cachedData);
   }
 
   const url = `${API_BASE_URL}competitions/${league}/matches`;
@@ -23,7 +35,11 @@ const fetchAllMatches = async (req, res) => {
       (match) => match.status === "TIMED"
     );
 
-    return res.status(200).json({ ...response.data, matches: upcomingMatches });
+    const responseData = { ...response.data, matches: upcomingMatches };
+
+    cache.set(cacheKey, responseData);
+
+    return res.status(200).json(responseData);
   } catch (error) {
     console.error("Error fetching from api: ", error);
     return res.status(500).json({ message: "Error while get data from api" });
