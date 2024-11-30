66aab18512aceccf945ebcedcf06fc504ff52d46
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-11-30 10:38:45 +0100

    Fixed matchModel so result is not required

--- backend/routes/matchRoutes.js
+++ backend/routes/matchRoutes.js
@@ -5,4 +5,15 @@ const router = express.Router();
 
 router.get("/", matchControllers.fetchAllMatches);
 
+// Denna är endast under utveckling och skall tas bort sen.
+router.get("/update-finished-matches", async (req, res) => {
+  try {
+    await matchControllers.updateMatchResultsAndPredicitons();
+    res.status(200).json({ message: "Match results updated successfully!" });
+  } catch (error) {
+    console.error("Error updating match results manually:", error);
+    res.status(500).json({ message: "Error updating match results" });
+  }
+});
+
 export default router;
