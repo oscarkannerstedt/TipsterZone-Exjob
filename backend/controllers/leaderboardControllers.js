8b2e7807099cfd3223e6382102da2f2a6f671f94
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-11-30 11:20:07 +0100

    Added - cache to leaderboard

--- backend/controllers/leaderboardControllers.js
+++ backend/controllers/leaderboardControllers.js
@@ -1,8 +1,20 @@
 import userModel from "../models/userModel.js";
+import NodeCache from "node-cache";
+
+// cache updates every 10 minutes
+const cache = new NodeCache({ stdTTL: 600 });
 
 //Get top 30 users based on total points.
 export const getLeaderboard = async (req, res) => {
   try {
+    //Check if leaderborad exist in cache
+    const cacheLeaderboard = cache.get("leaderboard");
+
+    if (cacheLeaderboard) {
+      console.log("Leaderboard from cache");
+      res.status(200).json(cacheLeaderboard);
+    }
+
     const topUsers = await userModel
       .find({})
       .sort({ total_points: -1 })
@@ -13,6 +25,9 @@ export const getLeaderboard = async (req, res) => {
       username: user.username,
       total_points: user.total_points,
     }));
+
+    //Save leaderboard in cache
+    cache.set("leaderboard", leaderboard);
 
     res.status(200).json(leaderboard);
   } catch (error) {
