1793eb1c59cacfbabd57f4ec792d8e7b99ec6899
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-07 12:46:31 +0100

    Added - Layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 export const Layout = () => {
-  return <>Layout</>;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 };
 
 export default Layout;
