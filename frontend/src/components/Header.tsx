42284f0618048c5d1a9590847d9db83b28d6e2b2
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-08 12:32:20 +0100

    Added - header navigation

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,56 @@
+import { useState } from "react";
+import { useHandleNavigation } from "../utils/navigationUtils";
+
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const isLoggedIn = false;
+
+  const handleNavigation = useHandleNavigation();
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <header className="header">
       <div className="header-container">
-        <div className="logo">TIPSTERZONE</div>
-        <div className="burger" onClick={toggleMenu}>
+        <div className="logo" onClick={() => handleNavigation("/")}>
+          TIPSTERZONE
+        </div>
+        <div
+          className="burger-menu"
+          onClick={toggleMenu}
+          aria-expanded={menuOpen}
+          role="button"
+          aria-label="Öppna meny"
+        >
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      <nav></nav>
+      <nav className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li onClick={() => handleNavigation("/matches")}>Matcher</li>
+          <li onClick={() => handleNavigation("/leaderboard")}>Topp Lista</li>
+          <li onClick={() => handleNavigation("/mypredictions")}>
+            Mina Tippningar
+          </li>
+
+          {isLoggedIn ? (
+            <li>
+              <button onClick={() => alert("Loggar ut...")}>Logga ut</button>
+            </li>
+          ) : (
+            <>
+              <li onClick={() => handleNavigation("/login")}>Logga In</li>
+              <li onClick={() => handleNavigation("/signup")}>
+                Skapa Användare
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
   );
 };
