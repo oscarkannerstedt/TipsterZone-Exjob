e4ae20b9f31a58c7e640b9ff6a6113f58effc0cf
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-10 10:14:07 +0100

    Added - login function

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,56 @@
+import { useState } from "react";
+import { loginUser } from "../services/authServices";
+import { AxiosError } from "axios";
+
 const Login = () => {
-  return <>Logga in</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const userData = await loginUser(email, password);
+      console.log("Inloggad som: ", userData);
+      //Navigate to new page
+    } catch (err: unknown) {
+      const axiosError = err as AxiosError<{ message: string }>;
+      if (axiosError.response && axiosError.response.data) {
+        setError(axiosError.response.data.message);
+      } else {
+        setError("Error while login");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Logga in</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Lösenord</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Logga in</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
 };
 
 export default Login;
