31c0d00b6b2f54ff8b2be4e423c6720a3268a489
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-13 11:54:55 +0100

    Changed - endpoint for login

--- frontend/src/services/authServices.ts
+++ frontend/src/services/authServices.ts
@@ -4,7 +4,10 @@ const API_URL = "https://tipsterzone-exjob.onrender.com";
 
 export const loginUser = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, { email, password });
+    const response = await axios.post(`${API_URL}/api/users/login`, {
+      email,
+      password,
+    });
     return response.data;
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
