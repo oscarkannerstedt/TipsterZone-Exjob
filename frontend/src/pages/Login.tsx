4f928b4b4640cc5ac8ab716c957019d11c7836a8
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-13 10:01:15 +0100

    Added - aria-label for login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,6 +14,11 @@ const Login = () => {
     e.preventDefault();
     setError(null);
 
+    if (!email.trim() || !password.trim()) {
+      setError("Du måste fylla i både e-post och användarnamn.");
+      return;
+    }
+
     try {
       const userData = await loginUser(email, password);
       console.log("Inloggad som: ", userData);
@@ -23,7 +28,7 @@ const Login = () => {
       if (axiosError.response && axiosError.response.data) {
         setError(axiosError.response.data.message);
       } else {
-        setError("Fel användarnamn eller lösenord");
+        setError("Fel användarnamn eller lösenord.");
       }
     }
   };
@@ -33,33 +38,46 @@ const Login = () => {
       <h1>Logga in</h1>
       <form onSubmit={handleSubmit}>
         <div className="email-wrapper">
-          <label>Email</label>
+          <label htmlFor="email">Email</label>
           <input
+            id="email"
             type="email"
+            placeholder="E-post"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
 
         <div className="password-wrapper">
-          <label>Lösenord</label>
+          <label htmlFor="password">Lösenord</label>
           <input
+            id="password"
             type="password"
+            placeholder="Lösenord"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
         </div>
         <button type="submit" className="submit-button">
           Logga in
         </button>
       </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      <div aria-live="polite">
+        {error && (
+          <p role="alert" style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
+      </div>
 
       <div className="create-user-wrapper">
         <p>Har du inte ett konto?</p>
         <button
           onClick={() => handleNavigation("/signup")}
           className="signup-button"
+          aria-label="Skapa ett konto om du inte redan har ett."
         >
           Skapa konto
         </button>
