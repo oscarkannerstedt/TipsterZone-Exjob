39aef1197618c0aeb2ded8b37ada75c96a8e76bb
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-14 12:47:26 +0100

    Added - cron job

--- backend/jobs/updateMatchResultsTasks.js
+++ backend/jobs/updateMatchResultsTasks.js
@@ -2,9 +2,9 @@ import cron from "node-cron";
 import { updateMatchResultsAndPredicitons } from "../controllers/matchControllers.js";
 
 // Scheduled to run every two hours
-// cron.schedule("0 */2 * * *", async () => {
-//   await updateMatchResultsAndPredicitons();
-// });
+cron.schedule("0 */2 * * *", async () => {
+  await updateMatchResultsAndPredicitons();
+});
 
 // Scheduled to run every 10 minutes
 // cron.schedule("*/3 * * * *", async () => {
