7cedacd6d01183f555c5146c4ee83269fb91f086
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-01-27 14:53:35 +0100

    Added - styling for home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div className="home-container">
-      <section className="heading-container">
+      <section className="heading-section">
         <h2>Välkommen till Tipsterzone - Ta del av de bästa speltipsen!</h2>
         <p>
           Här kan du ta del av speltips från topp tipparna, följa kommande
@@ -10,14 +10,16 @@ export const Home = () => {
         </p>
       </section>
 
-      <section className="text-image-container">
+      <section className="features-section">
         <h3>Varför välja tipsterzone?</h3>
-        <div className="">
-          <div className="">
+        <div className="features-grid">
+          <div className="feature-item">
             <img
               src="/images/stadium_image.jpg"
               alt="En fullsatt fotbolls arena."
               loading="lazy"
+              height="150"
+              width="300"
             />
             <h4>Ta del av top 30 tipparnas speltips</h4>
             <p>
@@ -25,11 +27,13 @@ export const Home = () => {
               att förbättra dina egna beslut och strategier.
             </p>
           </div>
-          <div className="">
+          <div className="feature-item">
             <img
               src="/images/soccer-fans-sitting-bar.jpg"
               alt="Ett gäng kompisar som sitter på en bar och kollar fotboll tillsammans."
               loading="lazy"
+              height="150"
+              width="300"
             />
             <h4>Tävla med vänner</h4>
             <p>
@@ -38,11 +42,13 @@ export const Home = () => {
               Spänning och gemenskap i ett!
             </p>
           </div>
-          <div className="">
+          <div className="feature-item">
             <img
               src="/images/leaderboard.jpg"
               alt="2 personer som analyserar statistik på en surfplatta."
               loading="lazy"
+              height="150"
+              width="300"
             />
             <h5>Håll koll på kommande matcher</h5>
             <p>
