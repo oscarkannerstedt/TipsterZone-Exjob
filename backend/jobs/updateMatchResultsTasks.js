29aa4de91ed6f80e6d227924335d63f21efc6953
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2025-02-08 15:09:57 +0100

    Changed - cron job to run 1 time/day

--- backend/jobs/updateMatchResultsTasks.js
+++ backend/jobs/updateMatchResultsTasks.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { updateMatchResultsAndPredicitons } from "../controllers/matchControllers.js";
 
-// Scheduled to run 01.00, 15.00, 18.00 & 21.00 every day
-cron.schedule("0 1,15,18,21 * * *", async () => {
+// Scheduled to run 01.00 & 18.00 every day
+cron.schedule("0 0 * * *", async () => {
   await updateMatchResultsAndPredicitons();
 });
