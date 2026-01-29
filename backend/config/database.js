ae0caf0c3c057c44d258085f9a354c54e8754e6d
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2026-01-29 09:20:25 +0100

    Uppdated - database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("connect to database");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database");
   } catch (error) {
     console.error("Error connecting to database", error);
   }
